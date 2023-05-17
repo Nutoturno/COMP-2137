09772c4deb4fc9f8c9d879aa9dcb6a6053ebb851
Jamie <Nutoturno.github.com>
2023-05-17 14:20:50 -0400

    update/upgrade script

---
+++ system-update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#created may 17 by jamie to perform a system update task for the lab
+
+#update the software cache in case it is needed
+sudo apt update
+#upgrade using new software package versions
+sudo apt upgrade -y
+
+
