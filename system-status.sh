de736344228fb071d6427dbfeef9c0afc91d3da2
Jamie <Nutoturno.github.com>
2023-06-06 23:49:56 -0400

    system status script

---
+++ system-status.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+#created by jamie to perform system status information
+
+#display the current cpu acitvity level
+
+lscpu
+
+#display the free memory
+
+free -h
+
+#display free disk space
+
+df -h
