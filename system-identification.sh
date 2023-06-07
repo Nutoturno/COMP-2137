e31c2d5e92e08ac225df3fa5fda97a2e6de65cbc
Jamie <Nutoturno.github.com>
2023-06-06 23:37:04 -0400

    system indentification script

---
+++ system-identification.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+#created by jamie to perform system identification information
+
+#display the current hostname
+
+hostname
+
+#display the ip address
+
+ip address show
+
+#display geteway ip
+
+ip route
+
+#shows if computer is online or offline
+
